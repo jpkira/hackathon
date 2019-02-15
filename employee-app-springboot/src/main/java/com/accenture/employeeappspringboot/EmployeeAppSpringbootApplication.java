edb7b39b143df9abdc1a865b6bce1ca38d453c0b
John Patrick Dizon <jpdestiny@gmail.com>
2019-02-15 18:54:27 +0800

    Added Springboot project

---
+++ employee-app-springboot/src/main/java/com/accenture/employeeappspringboot/EmployeeAppSpringbootApplication.java
@@ -0,0 +1,14 @@
+package com.accenture.employeeappspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeAppSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeAppSpringbootApplication.class, args);
+	}
+
+}
+
---
+++ employee-app-springboot/src/test/java/com/accenture/employeeappspringboot/EmployeeAppSpringbootApplicationTests.java
@@ -0,0 +1,17 @@
+package com.accenture.employeeappspringboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EmployeeAppSpringbootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
